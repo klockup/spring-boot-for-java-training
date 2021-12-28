38730e0d4f618e24cc0fdbed1a629ddd927fc6d3
cxy <kennysimple@163.com>
2021-12-28 22:42:54 +0800

    首次提交，springBoot项目pom

---
+++ src/main/java/com/train/trainingjavaspringboot/TrainingJavaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.train.trainingjavaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingJavaSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingJavaSpringbootApplication.class, args);
+    }
+
+}
