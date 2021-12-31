ad6186c12f2fbcf50de102626d2b3e0b731e70af
cxy <kennysimple@163.com>
2021-12-31 14:29:43 +0800

    线程起步

---
+++ src/main/java/com/train/all/roadtocoding/RoadToCoding.md
@@ -0,0 +1,3 @@
+# 该包中是我在 Road To coding 中学习，使用的
+地址为：https://www.r2coding.com/#/README?id=%e7%bc%96%e7%a8%8b%e8%af%ad%e8%a8%80
+
---
+++ src/main/java/com/train/all/roadtocoding/concurrent/ThreadDemo.java
@@ -0,0 +1,49 @@
+package com.train.all.roadtocoding.concurrent;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 线程 Demo
+ *
+ * 线程有两种实现
+ * 1 继承 Thread类
+ * 2 实现 Runnable接口
+ *
+ * @author: cxy
+ * @date: 2021/12/31
+ */
+@Slf4j
+public class ThreadDemo {
+
+    public static class MyThreadExThread extends Thread{
+        @Override
+        public void run() {
+            log.info("this Thread extends Thread");
+        }
+    }
+
+    public static class MyThreadImRunnable implements Runnable{
+
+        @Override
+        public void run() {
+            log.info("this Thread implements Runnable");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Thread myThread1=new MyThreadExThread();
+        myThread1.start();
+        Thread myThread2= new Thread(new MyThreadImRunnable());
+        myThread2.start();
+        //此时可以看到 命令行中打出的是线程的提示，如果直接调用 run方法，则不是线程的调用方式。
+        myThread2.run();
+
+        //java8的函数式编程，还有一种简便的方式创建线程
+        new Thread(()->{
+            log.info("函数式编程，不需要事先创建声明线程类");
+        }).start();
+
+    }
+
+}
