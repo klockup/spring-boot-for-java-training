5b6bd92ce1bf9332367e268714857257ec55d7ad
cxy <kennysimple@163.com>
2022-02-02 22:36:30 +0800

    d

--- src/main/java/com/train/all/TrainingJavaSpringbootApplication.java
+++ src/main/java/com/train/all/TrainingJavaSpringbootApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TrainingJavaSpringbootApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(TrainingJavaSpringbootApplication.class, args);
     }
